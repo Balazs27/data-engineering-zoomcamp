0a97a114b4e169e235bb53759e6dcc313826af60
Balázs Illovai <150837574+Balazs27@users.noreply.github.com>
2026-07-07 11:23:41 +0000

    docker compose

--- pipeline/ingest_data_cli.py
+++ pipeline/ingest_data_cli.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 
 dtype = {
@@ -31,25 +32,23 @@ parse_dates = [
 ]
 
 
-### INGESTING THE DATA IN CHUNKS
-def run():
-    pg_user = 'root'
-    pg_password = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_database = 'ny_taxi'
-
-    year = 2021
-    month = 1
-
-    target_table = 'yellow_taxi_data'
-
-    chunksize = 100000
-
+@click.command()
+@click.option('--pg-user', default='root', show_default=True, help='PostgreSQL user')
+@click.option('--pg-password', default='root', show_default=True, help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', show_default=True, help='PostgreSQL host')
+@click.option('--pg-port', default=5432, show_default=True, type=int, help='PostgreSQL port')
+@click.option('--pg-database', default='ny_taxi', show_default=True, help='PostgreSQL database name')
+@click.option('--target-table', default='yellow_taxi_data', show_default=True, help='Target table name')
+@click.option('--year', default=2021, show_default=True, type=int, help='Taxi data year')
+@click.option('--month', default=1, show_default=True, type=int, help='Taxi data month')
+@click.option('--chunksize', default=100000, show_default=True, type=int, help='Number of rows per chunk')
+def run(pg_user, pg_password, pg_host, pg_port, pg_database, target_table, year, month, chunksize):
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = prefix + f'yellow_tripdata_{year}-{month:02d}.csv.gz'
 
-    engine = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_database}')
+    engine = create_engine(
+        f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_database}'
+    )
 
     df_iter = pd.read_csv(
         url,
@@ -61,22 +60,24 @@ def run():
 
     # ITERATE OVER CHUNKS AND INSERT DATA
     first = True
+
     for df_chunk in tqdm(df_iter):
         if first:
             df_chunk.head(n=0).to_sql(
-                name=target_table, 
-                con=engine, 
+                name=target_table,
+                con=engine,
                 if_exists='replace',
                 index=False
             )
             first = False
 
         df_chunk.to_sql(
-            name=target_table, 
-            con=engine, 
+            name=target_table,
+            con=engine,
             if_exists='append',
             index=False
         )
+
 
 if __name__ == '__main__':
     run()
